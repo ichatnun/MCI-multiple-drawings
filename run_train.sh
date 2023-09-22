12be59c13b0cc5720d2023df90ed528fe1d65ac3
ichatnun <kanoon31@gmail.com>
2023-09-22 20:42:17 +0700

    Remove debugger

--- run_train.sh
+++ run_train.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-python main.py --random_seed=777 --include_clock --label_type="hard" --exp_name="clock_vgg16_hard_seed777" --num_epochs=100 --use_pretrained_weight 
+python main.py --random_seed=777 --include_clock --label_type="hard" --exp_name="clock_vgg16_hard_seed777" --model_name="vgg16" --num_epochs=100 --use_pretrained_weight 
+
+python main.py --random_seed=777 --include_clock --label_type="soft" --exp_name="clock_vgg16_soft_seed777" --model_name="vgg16" --num_epochs=100 --use_pretrained_weight 
