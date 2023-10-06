84f49efba413b16da5ca6534ef0d8a7539292eda
ichatnun <kanoon31@gmail.com>
2023-10-06 16:19:13 +0700

    Add positional encoding to the conv-att model

--- models/get_conv_att.py
+++ models/get_conv_att.py
@@ -1,4 +1,5 @@
-import torch, pdb
+import math
+import torch
 from torch.nn import Sequential, Conv2d, Linear, Parameter, TransformerEncoder, TransformerEncoderLayer
 from .get_vgg16 import VGG16Backbone
     
@@ -27,6 +28,9 @@ class multiInputConvAtt(torch.nn.Module):
         
         # Create a 1x1 convolution that adjusts the number of channels of the vector returned from the backbone
         self.conv1d = Conv2d(backbone_num_channels, self.hidden_dim_attn, kernel_size=1)
+        
+        # Creat the positional encoding
+        self.pos_encoder = PositionalEncoding(self.hidden_dim_attn, dropout=0.1, max_num_tokens=128)
           
         # Create the [CLS] token (shared between the tasks): (batch, seq, feature)
         self.CLS_token = Parameter(torch.randn(1, 1, self.hidden_dim_attn))
@@ -66,6 +70,9 @@ class multiInputConvAtt(torch.nn.Module):
             # Add the [CLS] token->(batch,seq,feature)=(batch,1+64,128)
             x_tokens = torch.cat((self.CLS_token.tile(x_tokens.shape[0], 1, 1), x_tokens), dim=1)
             
+            # Add positional encoding to the tokens
+            x_tokens = self.pos_encoder(x_tokens)
+            
             # Perform self attention->(batch,seq,feature)=(batch,1+64,128)
             x_self_attn = self.transformer_enc(x_tokens)
             
@@ -80,3 +87,24 @@ class multiInputConvAtt(torch.nn.Module):
     
         # Pass the concatenated results to the linear classifier (without softmax)
         return self.final(x_concat)
+    
+
+# Taken and slightly modified from https://pytorch.org/tutorials/beginner/transformer_tutorial.html
+class PositionalEncoding(torch.nn.Module):
+
+    def __init__(self, d_model: int, dropout: float = 0.1, max_num_tokens: int = 5000):
+        super().__init__()
+        self.dropout = torch.nn.Dropout(p=dropout)
+
+        position = torch.arange(max_num_tokens).unsqueeze(1)
+        div_term = torch.exp(torch.arange(0, d_model, 2) * (-math.log(10000.0) / d_model))
+        pe = torch.zeros(1, max_num_tokens, d_model)
+        pe[0,:,0::2] = torch.sin(position * div_term) # Even
+        pe[0,:,1::2] = torch.cos(position * div_term) # Odd
+        self.register_buffer('pe', pe)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        # x: (batch, seq, embedding_dim)
+        
+        x = x + self.pe[:,:x.size(1),:]
+        return self.dropout(x)
